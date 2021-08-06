7924ed5962983c89b8bad365f5767560a878a405
James Wells <jwells@dragonheim.net>
2021-08-06 16:10:01 -0700

    fix: updating Go & Alpine versions

--- tfenv/main.tf
+++ tfenv/main.tf
@@ -2,7 +2,7 @@
 module "us-east-1" {
 	source    = "./cluster"
 	region    = "us-east-1"
-	provider_alias = us-west-2
+	provider_alias = "us-east-1"
 	providers = {
 		aws = "aws.us-east-1"
 	}
@@ -11,7 +11,7 @@ module "us-east-1" {
 module "us-west-2" {
 	source    = "./cluster"
 	region    = "us-west-2"
-	provider_alias = us-west-2
+	provider_alias = "us-west-2"
 	providers = {
 		aws = "aws.us-west-2"
 	}
