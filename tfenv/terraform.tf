0557021282215c11e30e35e090936ed10b9caa9c
James Wells <jwells@dragonheim.net>
2021-09-18 20:20:26 -0700

    fix: Upgrading Terraform and starting to extend.

--- tfenv/terraform.tf
+++ tfenv/terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-	required_version = ">= 1.0.0"
-	required_providers {
-	aws = {
-		source  = "hashicorp/aws"
-		version = "~> 3.46.0"
-		}
-	}
+  required_version = ">= 1.0.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.59.0"
+    }
+  }
 }
